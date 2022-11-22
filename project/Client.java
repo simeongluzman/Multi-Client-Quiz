0f23ced1d650a70534fa6d4ebb715e2ea8432f28
simeongluzman <simeongluzman@gmail.com>
2022-11-22 12:25:38 -0500

    Template for Multi-thread Model
    added bases for client and server for us to work on.
    
    Added feature to keep track of username/score in hashmap in server class.
    
    for testing, the server prints the score map after each new client connects

--- project/Client.java
+++ project/Client.java
@@ -1,5 +1,61 @@
 package project;
 
-public class Client{
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+// Client class
+class Client {
+	
+	// driver code
+	public static void main(String[] args)
+	{
+		// establish a connection by providing host and port
+		// number
+		
+		System.out.println("Please Enter your Username without Spaces");
+		Scanner nameScanner = new Scanner(System.in);
+		String uName = nameScanner.nextLine();
+		
+		
+		
+		try (Socket socket = new Socket("localhost", 1234)) {
+			
+			// writing to server
+			PrintWriter out = new PrintWriter(
+				socket.getOutputStream(), true);
+
+			// reading from server
+			BufferedReader in
+				= new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+
+			// object of scanner class
+			Scanner sc = new Scanner(System.in);
+			String line = null;
+			
+			out.println(uName);
+
+			while (!"exit".equalsIgnoreCase(line)) {
+				
+				// reading from user
+				line = sc.nextLine();
+
+				// sending the user input to server
+				
+				out.println(line);
+				out.flush();
+
+				// displaying server reply
+				System.out.println("Server replied "
+								+ in.readLine());
+			}
+			
+			// closing the scanner object
+			sc.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- project/Server.java
+++ project/Server.java
@@ -1,5 +1,128 @@
 package project;
 
-public class Server{
-    
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// Server class
+class Server {
+	
+	static HashMap<String, Integer> scores = new HashMap<>();
+	
+	public static void main(String[] args){
+		
+		ServerSocket server = null;
+		
+		
+
+		try {
+
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			server.setReuseAddress(true);
+
+			// running infinite loop for getting
+			// client request
+			while (true) {
+
+				// socket object to receive incoming client
+				// requests
+
+				
+				Socket client = server.accept();
+				
+				
+				// Displaying that new client is connected
+				// to server
+				System.out.println("New client connected"
+								+ client.getInetAddress()
+										.getHostAddress());
+				
+				
+
+				// create a new thread object
+				ClientHandler clientSock
+					= new ClientHandler(client);
+
+				// This thread will handle the client
+				// separately
+				new Thread(clientSock).start();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	// ClientHandler class
+	private static class ClientHandler implements Runnable {
+		private final Socket clientSocket;
+
+		// Constructor
+		public ClientHandler(Socket socket)
+		{
+			this.clientSocket = socket;
+		}
+
+		public void run()
+		{
+			PrintWriter out = null;
+			BufferedReader in = null;
+			try {
+					
+				// get the outputstream of client
+				out = new PrintWriter(
+					clientSocket.getOutputStream(), true);
+
+				// get the inputstream of client
+				in = new BufferedReader(
+					new InputStreamReader(
+						clientSocket.getInputStream()));
+				
+				
+				
+				scores.put(in.readLine(),0);
+				System.out.println(scores);
+				String line;
+				while ((line = in.readLine()) != null) {
+
+					// writing the received message from
+					// client
+					System.out.printf(
+						" Sent from the client: %s\n",
+						line);
+					out.println(line);
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (out != null) {
+						out.close();
+					}
+					if (in != null) {
+						in.close();
+						clientSocket.close();
+					}
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
+
